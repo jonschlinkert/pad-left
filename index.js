176fe4d6359f55b6a5b494208b643b610c25fc4a
jonschlinkert <dev@sellside.com>
2014-12-05 08:06:53 -0500

    simplify

--- index.js
+++ index.js
@@ -10,17 +10,5 @@
 var repeat = require('repeat-string');
 
 module.exports = function padLeft(val, num, str) {
-  var padding = '';
-
-  // Breakpoints based on benchmarks to use the fastest approach
-  // for the given number of zeros
-  if (num <= 5 && !str) {
-    padding = '00000';
-  } else if (num <= 25 && !str) {
-    padding = '000000000000000000000000000';
-  } else {
-    return repeat(str || '0', num) + val;
-  }
-
-  return (padding + val).slice(-num - val.length);
+  return repeat(str || '0', num) + val;
 };
