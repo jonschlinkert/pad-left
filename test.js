ca5ad3bd24fdbe9576c4681e753d3d9d800c41e8
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-19 00:50:14 -0500

    build docs, update tests

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /*!
  * pad-left <https://github.com/jonschlinkert/pad-left>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Copyright (c) 2014-2015, Jon Schlinkert.
  * Licensed under the MIT License
  */
 
@@ -16,9 +16,9 @@ describe('pad left', function () {
   });
 
   it('should pad the specified number of zeros.', function () {
-    pad('abc', 10).should.equal('0000000000abc');
-    pad('abc', 100).should.equal('0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000abc');
-    pad('abc', 300).should.equal('000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000abc');
+    pad('abc', 10).should.equal('          abc');
+    pad('abc', 100).should.equal('                                                                                                    abc');
+    pad('abc', 300).should.equal('                                                                                                                                                                                                                                                                                                            abc');
   });
 
   it('should pad using the given string.', function () {
