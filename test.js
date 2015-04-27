c126401eaeafe98dd48bb9453223994ceba0a73d
jonschlinkert <jon.schlinkert@sellside.com>
2015-04-26 23:44:11 -0400

    adds tests for character

--- test.js
+++ test.js
@@ -13,6 +13,7 @@ var pad = require('./');
 describe('pad left', function () {
   it('should return the string when no padding amount is passed.', function () {
     pad('abc').should.equal('abc');
+    pad('abc').should.equal('abc', 0);
   });
 
   it('should pad the specified number of zeros.', function () {
@@ -21,10 +22,13 @@ describe('pad left', function () {
     pad('abc', 300).should.equal('                                                                                                                                                                                                                                                                                                            abc');
   });
 
-  it('should pad using the given string.', function () {
-    pad('abc', 10, '~').should.equal('~~~~~~~~~~abc');
-    pad('abc', 100, '~').should.equal('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~abc');
-    pad('abc', 300, '~').should.equal('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~abc');
+  it('should use the given character for padding.', function () {
+    var str = 'abcde';
+    pad(str, 10 - str.length, 0).should.equal('00000abcde');
+    pad(str, 10, '0').should.equal('0000000000abcde');
+    pad(str, 10, '~').should.equal('~~~~~~~~~~abcde');
+    pad(str, 100, '~').should.equal('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~abcde');
+    pad(str, 300, '~').should.equal('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~abcde');
   });
 });
 
