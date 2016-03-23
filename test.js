2d0ab7e33b7249e714e34f84a4e1ee2bb35ba1ed
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-22 22:23:24 -0400

    run `update && verb --init`

--- test.js
+++ test.js
@@ -10,19 +10,19 @@
 var should = require('should');
 var pad = require('./');
 
-describe('pad left', function () {
-  it('should return the string when no padding amount is passed.', function () {
+describe('pad left', function() {
+  it('should return the string when no padding amount is passed.', function() {
     pad('abc').should.equal('abc');
     pad('abc').should.equal('abc', 0);
   });
 
-  it('should pad the specified number of zeros.', function () {
+  it('should pad the specified number of zeros.', function() {
     pad('abc', 10).should.equal('       abc');
     pad('abc', 100).should.equal('                                                                                                 abc');
     pad('abc', 300).should.equal('                                                                                                                                                                                                                                                                                                         abc');
   });
 
-  it('should use the given character for padding.', function () {
+  it('should use the given character for padding.', function() {
     var str = 'abcde';
     pad(str, 10, 0).should.equal('00000abcde');
     pad(str, 10, '0').should.equal('00000abcde');
@@ -31,7 +31,7 @@ describe('pad left', function () {
     pad(str, 300, '~').should.equal('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~abcde');
   });
   
-  it('should coerce number to string before padding.', function () {
+  it('should coerce number to string before padding.', function() {
     pad(123).should.equal('123');
     pad(123, 5).should.equal('  123');
     pad(123, 10, '0').should.equal('0000000123');
